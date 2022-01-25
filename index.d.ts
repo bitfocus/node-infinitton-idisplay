78a90e374bc31344c7a795e03ae7b92780f63cec
Julian Waller <git@julusian.co.uk>
2022-01-25 20:09:24 +0000

    fix: add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+import EventEmitter = require("events");
+
+declare class Infinitton extends EventEmitter {
+    /**
+     * The pixel size of an icon written to the Infinitton key.
+     */
+    static get ICON_SIZE(): number;
+   
+    constructor(devicePath: string);
+
+    /**
+     * Fills the given key with a solid color.
+     *
+     * @param {number} keyIndex The key to fill 0 - 14
+     * @param {number} r The color's red value. 0 - 255
+     * @param {number} g The color's green value. 0 - 255
+     * @param {number} b The color's blue value. 0 -255
+     */
+    fillColor(keyIndex: number, r: number, g: number, b: number): void;
+
+    /**
+     * Fills the given key with an image in a Buffer.
+     *
+     * @param {number} keyIndex The key to fill 0 - 14
+     * @param {Buffer} imageBuffer
+     */
+    fillImage(keyIndex: number, imageBuffer: Buffer): void;
+
+    /**
+     * Clears the given key.
+     *
+     * @param {number} keyIndex The key to clear 0 - 14
+     */
+    clearKey(keyIndex: number): void;
+
+    /**
+     * Clears all keys.
+     */
+    clearAllKeys(): void;
+
+    /**
+     * Sets the brightness of the keys on the Infinitton
+     *
+     * @param {number} percentage The percentage brightness
+     */
+    setBrightness(percentage: number): void;
+}
+
+export = Infinitton;
